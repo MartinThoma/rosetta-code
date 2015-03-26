e29dd306be29ea8c7be59c8de76c9bf67cd4c4d8
Martin Thoma <info@martin-thoma.de>
2015-03-26 16:06:04 +0100

    sorting/integer-array: Updated Go example

--- sorting/integer-array/go.go
+++ sorting/integer-array/go.go
@@ -1,27 +1,29 @@
 package main
 
-import "fmt"
-import "sort"
-import "math/rand"
-import "time"
+import (
+	"fmt"
+	"math/rand"
+	"sort"
+	"time"
+)
 
 func main() {
-    // Seed
-    rand.Seed( time.Now().UTC().UnixNano())
+	// Seed
+	rand.Seed(time.Now().UTC().UnixNano())
 
-    var slice []int
-    var n = 10000000
+	var slice []int
+	var n = 10000000
 
-    fmt.Printf("Start creating %v random integers...\n", n)
-    for i := 0; i < n; i++ {
-        slice = append(slice, rand.Intn(1000000000))
-    }
+	fmt.Printf("Start creating %v random integers...\n", n)
+	for i := 0; i < n; i++ {
+		slice = append(slice, rand.Intn(1000000000))
+	}
 
-    fmt.Printf("Start sorting %v random integers...\n", n)
+	fmt.Printf("Start sorting %v random integers...\n", n)
 
-    start := time.Now()
-    sort.Sort(sort.IntSlice(slice))
-    elapsed := time.Since(start)
+	start := time.Now()
+	sort.Sort(sort.IntSlice(slice))
+	elapsed := time.Since(start)
 
-    fmt.Printf("%s\n", elapsed)
+	fmt.Printf("%s\n", elapsed)
 }
